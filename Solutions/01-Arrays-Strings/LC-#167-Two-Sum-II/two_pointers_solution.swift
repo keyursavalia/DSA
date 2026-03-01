cc37b5a6ede9a4bfce462fd7ab42f12ebdeb019a
Keyur <savalia.keyur1091@gmail.com>
2026-03-01 00:31:00 -0800

    add: LC #167 - Two Sum II - two pointers solution

--- Solutions/01-Arrays-Strings/LC-#167-Two-Sum-II/two_pointers_solution.swift
+++ Solutions/01-Arrays-Strings/LC-#167-Two-Sum-II/two_pointers_solution.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// LC #167 - Two Sum II
-/// Optimized Approach using HashMap - 02/28/2026
+/// Optimized Approach using 2 Pointers - 02/28/2026
 /// Time: O(n) | Space: O(1)
 
 class Solution {
@@ -15,24 +15,25 @@ class Solution {
     ///     else an empty array
     func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
         
+        // initialize pointers
         var leftPointer: Int = 0
         var rightPointer: Int = numbers.count - 1
         
-        while leftPointer < rightPointer {
+        while leftPointer < rightPointer {                              // true until they they cross each other
             
-            let sum = numbers[leftPointer] + numbers[rightPointer]
+            let sum = numbers[leftPointer] + numbers[rightPointer]      // keep track of current sum
             
-            if sum == target {
-                return [leftPointer + 1, rightPointer + 1]
-            } else if sum < target {
-                leftPointer += 1
-            } else {
-                rightPointer -= 1
+            if sum == target {                                          /// if sum is equal to target:
+                return [leftPointer + 1, rightPointer + 1]              ///         return indices incremented by 1
+            } else if sum < target {                                    /// if sum is less than target:
+                leftPointer += 1                                        ///         increment left pointer
+            } else {                                                    /// if sum is greater than target:
+                rightPointer -= 1                                       ///         decrement right pointer
             }
             
         }
         
-        return []                                       // if no solution found, return empty array
+        return []                                                       // if no solution found, return empty array
     }
 }
 
